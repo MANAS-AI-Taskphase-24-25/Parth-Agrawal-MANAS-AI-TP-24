125d482d158dd11a1b9760734c936007e16f9b5b
parthagrawal234 <personal.parth.agr.234@outlook.com>
2025-04-05 18:47:14 +0530

    new

--- Task 7.0/a_star_planner/src/planner_node.cpp
+++ Task 7.0/a_star_planner/src/planner_node.cpp
@@ -45,13 +45,80 @@ private:
         }
     }
 
-    std::vector<std::pair<int, int>> aStarSearch(
-        const std::pair<int, int>& start,
-        const std::pair<int, int>& goal) {
-        
-        // TODO: Implement A* algorithm here
-        return {};
+    std::vector<std::pair<int, int>> aStarSearch(const std::pair<int, int>& start, const std::pair<int, int>& goal) {
+    struct Node {
+        int x, y;
+        float g, f;
+        bool operator>(const Node& other) const { return f > other.f; }
+    };
+
+    auto index = [this](int x, int y) {
+        return y * current_map_.info.width + x;
+    };
+
+    auto isValid = [this](int x, int y) {
+        return x >= 0 && y >= 0 &&
+               x < static_cast<int>(current_map_.info.width) &&
+               y < static_cast<int>(current_map_.info.height) &&
+               current_map_.data[index(x, y)] == 0;
+    };
+
+    std::priority_queue<Node, std::vector<Node>, std::greater<Node>> open;
+    std::unordered_map<int, std::pair<int, int>> came_from;
+    std::unordered_map<int, float> g_score;
+
+    auto h = [](int x1, int y1, int x2, int y2) {
+        return std::hypot(x2 - x1, y2 - y1);
+    };
+
+    int start_idx = index(start.first, start.second);
+    open.push({start.first, start.second, 0.0f, h(start.first, start.second, goal.first, goal.second)});
+    g_score[start_idx] = 0.0f;
+
+    std::vector<std::pair<int, int>> directions = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1},
+        {1, 1}, {-1, -1}, {1, -1}, {-1, 1}
+    };
+
+    while (!open.empty()) {
+        Node current = open.top();
+        open.pop();
+
+        if (current.x == goal.first && current.y == goal.second) {
+            std::vector<std::pair<int, int>> path;
+            int current_idx = index(goal.first, goal.second);
+            while (came_from.find(current_idx) != came_from.end()) {
+                auto [x, y] = came_from[current_idx];
+                path.emplace_back(x, y);
+                current_idx = index(x, y);
+            }
+            path.emplace_back(start.first, start.second);
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
+        for (const auto& dir : directions) {
+            int nx = current.x + dir.first;
+            int ny = current.y + dir.second;
+
+            if (!isValid(nx, ny)) continue;
+
+            float tentative_g = g_score[index(current.x, current.y)] + h(current.x, current.y, nx, ny);
+            int neighbor_idx = index(nx, ny);
+
+            if (g_score.find(neighbor_idx) == g_score.end() || tentative_g < g_score[neighbor_idx]) {
+                came_from[neighbor_idx] = {current.x, current.y};
+                g_score[neighbor_idx] = tentative_g;
+                float f_score = tentative_g + h(nx, ny, goal.first, goal.second);
+                open.push({nx, ny, tentative_g, f_score});
+            }
+        }
     }
+
+    RCLCPP_WARN(this->get_logger(), "No path found!");
+    return {};
+}
+
 };
 
 int main(int argc, char* argv[]) {
